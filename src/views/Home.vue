2c4d18b001d2868f940dd9878956f6e075c63932
strawhatrishabh <goyalrishabh75@gmail.com>
2022-11-14 16:58:02 +0530

    removed commented code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <!-- <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
-        <div class="bg-gray-400 w-96 h-96 relative z-0">
-            <p class="italic text-bold bd-red-100 font-serif">Map</p>
-            <div class="absolute inset-0 flex justify-center items-center z-10">
-            <p class="text-2xl font-bold">This should be on top of the map</p>
-            </div>
-        </div>
-    </div> -->
-    <!-- v-if="screenLoader" -->
     <div class="container h-full fixed block top-50 left-50 z-50" v-if="screenLoader">
         <div class="flex items-center justify-center ">
             <div class="w-16 h-16 border-b-2 border-gray-900 rounded-full animate-spin"></div>
